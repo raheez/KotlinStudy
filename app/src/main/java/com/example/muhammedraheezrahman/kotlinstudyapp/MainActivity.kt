3a3e254bf2d204b2f3d3e6a68412e87b18b63ba4
Muhammed raheez Rahman <muhammedraheezrahman@Muhammeds-MacBook-Pro.local>
2018-12-18 00:47:12 +0400

    initial commit

---
+++ app/src/main/java/com/example/muhammedraheezrahman/kotlinstudyapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.muhammedraheezrahman.kotlinstudyapp
+
+import  android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var  string : String;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var titleTv : TextView = findViewById(R.id.titleTv)
+
+        var yourstring : String = "hello world"
+        var myno = 20
+        butt.setOnClickListener {
+            Toast.makeText(applicationContext,"Displaying toast ",Toast.LENGTH_SHORT).show();
+        }
+
+        titleTv.setOnClickListener {
+            Toast.makeText(applicationContext,"Displaying toast ",Toast.LENGTH_SHORT).show();
+            titleTv.text = "Yo baby"
+
+             string = titleTv.text.toString()
+            title2Tv.text = string
+
+
+        }
+
+
+    }
+
+}
